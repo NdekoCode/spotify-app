5d37ce1753ad60e4eca1e8afe47d00b3c0b7484b
NdekoCode <arickbulakali@gmail.com>
2022-09-04 23:08:15 +0200

    refactor: title app and favicon

--- src/components/Musics.jsx
+++ src/components/Musics.jsx
@@ -6,15 +6,15 @@ const Musics = () => {
       <div>
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
           {/* CARD 1 */}
-          <div className="rounded-2xl p-3 bg-white shadow-sm">
+          <div className="rounded-2xl p-3 bg-white shadow-sm cursor-pointer">
             <div className="group relative">
               <img
                 className="w-full md:w-72 block rounded-2xl shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/en/f/f1/Tycho_-_Epoch.jpg"
                 alt=""
               />
-              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition duration-300 justify-evenly">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -26,7 +26,7 @@ const Musics = () => {
                     <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={40}
@@ -38,7 +38,7 @@ const Musics = () => {
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -59,15 +59,15 @@ const Musics = () => {
           </div>
           {/* END OF CARD 1 */}
           {/* CARD 2 */}
-          <div className="rounded-2xl p-3 bg-white shadow-sm">
+          <div className="rounded-2xl p-3 bg-white shadow-sm cursor-pointer">
             <div className="group relative">
               <img
                 className="w-full md:w-72 block rounded-2xl shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/en/c/ca/Tycho_-_Awake.png"
                 alt=""
               />
-              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition duration-300 justify-evenly">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -79,7 +79,7 @@ const Musics = () => {
                     <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={40}
@@ -91,7 +91,7 @@ const Musics = () => {
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -112,15 +112,15 @@ const Musics = () => {
           </div>
           {/* END OF CARD 2 */}
           {/* CARD 3 */}
-          <div className="rounded-2xl p-3 bg-white shadow-sm">
+          <div className="rounded-2xl p-3 bg-white shadow-sm cursor-pointer">
             <div className="group relative">
               <img
                 className="w-full md:w-72 block rounded-2xl shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/en/1/11/Dive_tycho_album.jpg"
                 alt=""
               />
-              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition duration-300 justify-evenly">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -132,7 +132,7 @@ const Musics = () => {
                     <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={40}
@@ -144,7 +144,7 @@ const Musics = () => {
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -166,15 +166,15 @@ const Musics = () => {
           {/* END OF CARD 3 */}
 
           {/* CARD 4 */}
-          <div className="rounded-2xl p-3 bg-white shadow-sm">
+          <div className="rounded-2xl p-3 bg-white shadow-sm cursor-pointer">
             <div className="group relative">
               <img
                 className="w-full md:w-72 block rounded-2xl shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/en/1/11/Dive_tycho_album.jpg"
                 alt=""
               />
-              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition duration-300 justify-evenly">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -186,7 +186,7 @@ const Musics = () => {
                     <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={40}
@@ -198,7 +198,7 @@ const Musics = () => {
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -220,15 +220,15 @@ const Musics = () => {
           {/* END OF CARD 4 */}
 
           {/* CARD 5 */}
-          <div className="rounded-2xl p-3 bg-white shadow-sm">
+          <div className="rounded-2xl p-3 bg-white shadow-sm cursor-pointer">
             <div className="group relative">
               <img
                 className="w-full md:w-72 block rounded-2xl shadow-2xl"
                 src="https://upload.wikimedia.org/wikipedia/en/1/11/Dive_tycho_album.jpg"
                 alt=""
               />
-              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+              <div className="absolute bg-black rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition duration-300 justify-evenly">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
@@ -240,7 +240,7 @@ const Musics = () => {
                     <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={40}
@@ -252,7 +252,7 @@ const Musics = () => {
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                   </svg>
                 </button>
-                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+                <button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
