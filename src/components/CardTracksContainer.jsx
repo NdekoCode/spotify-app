71b4d9b8f2ae28f38db4f2b233b3c186a2e90ee0
NdekoCode <arickbulakali@gmail.com>
2022-09-07 10:45:47 +0200

    refactor: artist card tracks and add they container

---
+++ src/components/CardTracksContainer.jsx
@@ -0,0 +1,195 @@
+import React from "react";
+
+const CardTracksContainer = () => {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-x-8  gap-y-2">
+      <a href="/track/1IAzD1muglOxOcPbUHs70R">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e02b6d4478c6f91f1cb2d326c78"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">Whatever You Like</h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">T.I.</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="/track/6L6lwZwGzn6WWqeLbONWZf">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e02caf2e6bf6b01a261d23822a4"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">Bloqué</h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">Fally Ipupa</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="/track/7GcphTvAmcb6GdoYjN4N8O">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e022488e22b1bbe6b321942864e"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">S U I S - M O I</h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">Fally Ipupa, Tayc</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="/track/0SZrATAQWfdDuufR76mi7Q">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e023a051f0234fa9cbc491ad2dd"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">Still Fly</h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">Big Tymers</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="/track/31kUJ6TBHOICknBrFugOvI">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e027454c8b62e42e482d06e022f"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">
+                Winner (feat. Justin Timberlake &amp; T.I.)
+              </h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">
+                  Jamie Foxx, Justin Timberlake, T.I.
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="/track/6XAA7T9VHcqKQSzjmXAJ8o">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e02128b948ac9a26c14269282d4"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">
+                That's All She Wrote
+              </h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">T.I., Eminem</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="/track/1m519DBMMI8x3EPGzSCBki">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e029a5b3a803e584e48ff2486d3"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">
+                Tant pis (feat. Dadju)
+              </h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">Dry, Dadju</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="/track/4sgnTQggcrgn2F8RFjShaH">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e023cc8ceca99e45ce4984eab3b"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">Mafuzzy Style</h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">Dadju</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="/track/6VIP3K7z1R5t7FWirkqc3U">
+        <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
+          <div className="px-4">
+            <div className="image pt-4 opacity-75">
+              <img
+                src="https://i.scdn.co/image/ab67616d00001e02926dbcf60c7126b8db67e2da"
+                alt=""
+                className="w-32 h-32 object-cover"
+              />
+            </div>
+            <div className="w-32">
+              <h3 className="mt-1 text-gray-400 truncate">Déjà trouvé</h3>
+              <div className="text-sm text-gray-700 mb-2">
+                <h3 className="truncate">Dadju</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a
+        className="bg-gradient-to-b from-gray-900 to-black mr-3 md:mr-6 mt-4 w-36 md:w-40 h-44 flex justify-center items-center text-gray-500 hover:text-gray-200 rounded"
+        href="/track"
+      >
+        <h2>See More</h2>
+      </a>
+    </div>
+  );
+};
+
+export default CardTracksContainer;
--- src/components/MusicApp.jsx
+++ src/components/MusicApp.jsx
@@ -4,6 +4,8 @@ import Sidebar from "./Sidebar";
 import MusicContext from "../data/AppContext";
 import ModalPlayer from "./ModalPlayer";
 import UserData from "./UserData";
+import GreetUser from "./GreetUser";
+import CardTracksContainer from "./CardTracksContainer";
 
 const MusicApp = memo(({ children }) => {
   const { idSong, showFrame, typePlay } = MusicContext();
@@ -18,17 +20,11 @@ const MusicApp = memo(({ children }) => {
             <UserData />
             <main className="py-8 md:pb-12">
               <div className="py-10">
-                <h1 className="text-center text-3xl sm:text-4xl lg:text-4xl">
-                  <span className="text-gray-500">Hi, </span>
-                  <span className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 text-transparent bg-clip-text">
-                    Arick Bulakali.
-                  </span>
-                  <span>👋</span>
-                </h1>
+                <GreetUser />
               </div>
               <div className="mt-10 md:mt-24">
                 <h2 className="text-2xl heading text-center sm:text-left ">
-                  Artists you love the most
+                  Artists you must love the most
                 </h2>
                 <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-x-4 gap-y-2">
                   <a
@@ -43,205 +39,7 @@ const MusicApp = memo(({ children }) => {
                 <h2 className="text-2xl heading text-center sm:text-left ">
                   Your most favourite tracks of all time
                 </h2>
-                <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-x-8  gap-y-2">
-                  <a href="/track/1IAzD1muglOxOcPbUHs70R">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e02b6d4478c6f91f1cb2d326c78"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            Whatever You Like
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">T.I.</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a href="/track/6L6lwZwGzn6WWqeLbONWZf">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e02caf2e6bf6b01a261d23822a4"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            Bloqué
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">Fally Ipupa</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a href="/track/7GcphTvAmcb6GdoYjN4N8O">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e022488e22b1bbe6b321942864e"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            S U I S - M O I
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">Fally Ipupa, Tayc</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a href="/track/0SZrATAQWfdDuufR76mi7Q">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e023a051f0234fa9cbc491ad2dd"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            Still Fly
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">Big Tymers</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a href="/track/31kUJ6TBHOICknBrFugOvI">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e027454c8b62e42e482d06e022f"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            Winner (feat. Justin Timberlake &amp; T.I.)
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">
-                              Jamie Foxx, Justin Timberlake, T.I.
-                            </h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a href="/track/6XAA7T9VHcqKQSzjmXAJ8o">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e02128b948ac9a26c14269282d4"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            That's All She Wrote
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">T.I., Eminem</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a href="/track/1m519DBMMI8x3EPGzSCBki">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e029a5b3a803e584e48ff2486d3"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            Tant pis (feat. Dadju)
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">Dry, Dadju</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a href="/track/4sgnTQggcrgn2F8RFjShaH">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e023cc8ceca99e45ce4984eab3b"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            Mafuzzy Style
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">Dadju</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a href="/track/6VIP3K7z1R5t7FWirkqc3U">
-                    <div className="bg-card pb-1.5 mr-3 md:mr-6 mt-4 w-36 md:w-40 shadow-2xl rounded-sm transition-transform duration-200 transform hover:scale-105">
-                      <div className="px-4">
-                        <div className="image pt-4 opacity-75">
-                          <img
-                            src="https://i.scdn.co/image/ab67616d00001e02926dbcf60c7126b8db67e2da"
-                            alt=""
-                            className="w-32 h-32 object-cover"
-                          />
-                        </div>
-                        <div className="w-32">
-                          <h3 className="mt-1 text-gray-400 truncate">
-                            Déjà trouvé
-                          </h3>
-                          <div className="text-sm text-gray-700 mb-2">
-                            <h3 className="truncate">Dadju</h3>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                  <a
-                    className="bg-gradient-to-b from-gray-900 to-black mr-3 md:mr-6 mt-4 w-36 md:w-40 h-44 flex justify-center items-center text-gray-500 hover:text-gray-200 rounded"
-                    href="/track"
-                  >
-                    <h2>See More</h2>
-                  </a>
-                </div>
+                <CardTracksContainer />
               </div>
               <div className="mt-10 md:mt-20 w-centerull sm:justify-start">
                 <h2 className="text-2xl heading text-center sm:text-left ">
