c5bb6e5fb65e19928ce9b32d0d301c1708be959e
NdekoCode <arickbulakali@gmail.com>
2022-09-10 18:59:00 +0200

    refactor: styles card albums

--- src/components/AlbumsData.jsx
+++ src/components/AlbumsData.jsx
@@ -47,7 +47,7 @@ function AlbumsData() {
             <h2 className="text-2xl heading text-center sm:text-left my-3 ">
               Suggest Albums of all time
             </h2>
-            <section className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4">
+            <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {items.map((album, key) => (
                 // eslint-disable-next-line react/no-array-index-key
                 <CardDetails album={album} key={key} />
@@ -68,7 +68,7 @@ function AlbumsData() {
             </span>{' '}
             Albums of all time
           </h2>
-          <section className="grid grid-cols-1 lg:grid-cols-2  xl:grid-cols-2 gap-4">
+          <section className="cards grid grid-cols-1 md:grid-cols-2 gap-4">
             {albums.map((album, index) => (
               // eslint-disable-next-line react/no-array-index-key
               <CardDetails album={album} key={index} />
--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -16,14 +16,16 @@ function CardDetails({ album }) {
   return (
     // <div className="bg-dark group py-4 sm:py-6 flex flex-col justify-center md:py-12">
     <div
-      className="sm:max-w-xl sm:mx-auto 	 rounded-lg sm:rounded-3xl overflow-hidden "
+      className="sm:max-w-xl sm:mx-auto 	 rounded-lg sm:rounded-3xl 
+ overflow-hidden "
       style={{ backgroundImage: `url(${album.images[0].url})` }}
     >
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a
         href="#"
         onClick={showPlayer}
-        className="cursor-pointer duration-300  bg-gray-800 shadow-lg  sm:max-h-80 p-5 md:p-8 flex flex-wrap sm:flex-nowrap space-x-8 sm:h-80 bg-transparent backdrop-blur h-full w-full"
+        className="group
+relative cursor-pointer duration-300  bg-gray-800 shadow-lg  sm:max-h-80 p-5 md:p-8 flex flex-wrap sm:flex-nowrap space-x-8 sm:h-80 bg-transparent backdrop-blur h-full w-full"
       >
         <div className="sm:h-48  w-full sm:w-1/2">
           <img
@@ -32,10 +34,27 @@ function CardDetails({ album }) {
             alt=""
           />
         </div>
-        <div className="group flex flex-col w-full mt-5 sm:mt-0 sm:w-1/2 space-y-4 relative">
+        <div className="absolute animate-pulse rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition duration-300 justify-evenly">
+          <button
+            type="button"
+            onClick={showPlayer}
+            className="hover:scale-110 text-green opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={40}
+              height={40}
+              fill="currentColor"
+              className="bi bi-play-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+            </svg>
+          </button>
+        </div>
+        <div className=" flex flex-col w-full mt-5 sm:mt-0 sm:w-1/2 space-y-4 ">
           <div className="flex justify-between items-start w-full sm:w-auto">
-            <h2 className="text-lg md:text-3xl font-bold">
-              {' '}
+            <h2 className="text-lg md:text-2xl font-bold break-words">
               {catString(name)}
             </h2>
             <div className="bg-yellow-400 font-bold rounded-xl p-2">
@@ -49,24 +68,6 @@ function CardDetails({ album }) {
             </div>
           </div>
 
-          <div className="absolute animate-pulse rounded-2xl bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition duration-300 justify-evenly">
-            <button
-              type="button"
-              onClick={showPlayer}
-              className="hover:scale-110 text-green opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition duration-300"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={40}
-                height={40}
-                fill="currentColor"
-                className="bi bi-play-circle-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-              </svg>
-            </button>
-          </div>
           <div className="flex text-xl md:text-2xl font-bold text-a">
             {catString(artists[0].name)}
           </div>
