418e145c9f9fa6b4208d28106dc63f0558e1bcd0
NdekoCode <arickbulakali@gmail.com>
2022-09-11 08:05:39 +0200

    remove unused called component

--- src/components/PlaylistsData.jsx
+++ src/components/PlaylistsData.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import CardDetails from './CardDetails';
 import MusicContext from '../data/AppContext';
 import useFetch from '../data/hookFunc';
 import CardPlaylist from './CardPlaylist';
