f8a961d01e935174b542c91b967fe47cae0e0c1f
NdekoCode <arickbulakali@gmail.com>
2022-09-12 14:22:23 +0200

    feat: custom skeleton on dashboard page

--- src/components/SkeletonData.jsx
+++ src/components/SkeletonData.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import CardSkeleton from './CardSkeleton';
 import '../assets/css/components/SkeletonData.css';
 
-const SkeletonData = () => {
+const SkeletonData = ({ parent = true }) => {
+  if (parent) {
+    return (
+      <section className="grid cards-grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+      </section>
+    );
+  }
   return (
-    <section className="grid cards-grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+    <>
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
-    </section>
+    </>
   );
 };
 
