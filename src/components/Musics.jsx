25897174a83120705c14e9a94c7e32e5b471d4d5
NdekoCode <arickbulakali@gmail.com>
2022-09-12 11:53:29 +0200

    feat: hide related on search and optimize loading view

--- src/components/Musics.jsx
+++ src/components/Musics.jsx
@@ -1,25 +1,30 @@
+import MusicContext from '../data/AppContext';
 import AlbumsData from './AlbumsData';
 import CardTracksContainer from './CardTracksContainer';
 import TrackData from './TrackData';
 // import TrackData from "./TrackDataTable";
 
 const Musics = () => {
+  const { searchUser } = MusicContext();
+
   return (
     <div className="grid place-items-center mt-5 bg-app ">
-      <div className="mt-10 md:mt-24">
-        <h2 className="text-2xl heading text-center sm:text-left ">
-          Artists you must love the most
-        </h2>
-        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-x-4 gap-y-2">
-          <a
-            className="bg-gradient-to-b from-gray-900 to-black mr-3 md:mr-6 mt-4 w-36 md:w-40 h-44 flex justify-center items-center text-gray-500 hover:text-gray-200 rounded"
-            href="/artists"
-          >
-            <h2>See More</h2>
-          </a>
+      {searchUser.length < 1 && (
+        <div className="mt-10">
+          <h2 className="text-2xl heading text-center sm:text-left ">
+            Artists you must love the most
+          </h2>
+          <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-x-4 gap-y-2">
+            <a
+              className="bg-gradient-to-b from-gray-900 to-black mr-3 md:mr-6 mt-4 w-36 md:w-40 h-44 flex justify-center items-center text-gray-500 hover:text-gray-200 rounded"
+              href="/artists"
+            >
+              <h2>See More</h2>
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="mt-10 md:mt-20">
+      )}
+      <div className="mt-10">
         <h2 className="text-2xl heading text-center sm:text-left ">
           Your most favourite tracks of all time
         </h2>
