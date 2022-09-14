f3a404ad2f43a4ef65a21c5b5c548d851525ae9c
NdekoCode <arickbulakali@gmail.com>
2022-09-14 15:59:03 +0200

    feat: add converter to followers data

--- src/data/utilsFunc.js
+++ src/data/utilsFunc.js
@@ -1,6 +1,6 @@
 export function catString(str, length = 20) {
   if (str.length > length) {
-    return str.substring(0, length) + "...";
+    return str.substring(0, length) + '...';
   }
   return str;
 }
@@ -13,17 +13,28 @@ export function setDataStorage(key, data) {
 
 export function verifyUserConnect() {
   if (
-    getDataStorage("userData") !== null &&
-    getDataStorage("userData") !== undefined
+    getDataStorage('userData') !== null &&
+    getDataStorage('userData') !== undefined
   ) {
-    return Object.keys(getDataStorage("userData")).length > 1;
+    return Object.keys(getDataStorage('userData')).length > 1;
   }
   return false;
 }
 
 export function connectedUser(newUser) {
-  setDataStorage("userData", newUser);
+  setDataStorage('userData', newUser);
 }
 export function disconnectedUser() {
-  localStorage.removeItem("userData");
+  localStorage.removeItem('userData');
+}
+function floatFormat(number) {
+  return Number.parseFloat(number).toFixed(1);
+}
+export function convertFollowersNumber(value) {
+  if (value > 1_000_000) {
+    return parseFloat(floatFormat(value / 1_000_000)) + 'M';
+  } else if (value > 1_000) {
+    return parseFloat(floatFormat(value / 1_000)) + 'K';
+  }
+  return value;
 }
